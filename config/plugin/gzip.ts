d95eecd5d6fd3f0377250f03a4be39cb999b453e
xutao1997 <xutao01@national-space.com>
2021-10-29 11:07:23 +0800

    chore: clear unused code

--- config/plugin/gzip.ts
+++ config/plugin/gzip.ts
@@ -1,5 +1,4 @@
 import gzipPlugin from 'rollup-plugin-gzip';
-// import {Plugin} from 'vite';
 
 export function configGzipPlugin(isBuild: boolean): any {
   if (isBuild) {
--- src/plugins/antd.ts
+++ src/plugins/antd.ts
@@ -1,17 +1,10 @@
 import type { App } from 'vue';
 import {
-  // Table,
-  // Card,
-  // Avatar,
   Select,
-  // Breadcrumb,
-  // Tag,
-  // Descriptions,
   Input,
   DatePicker,
   Upload,
   Button,
-  //Tree,
   Checkbox,
   Radio,
   Image,
@@ -26,19 +19,16 @@ import 'ant-design-vue/dist/antd.less'
 
 export default (app: App<Element>) => {
   app.use(Select)
-    //.use(Breadcrumb)
     .use(Button)
     .use(Input)
     .use(Radio)
     .use(DatePicker)
     .use(Upload)
-    //.use(Descriptions)
     .use(Modal)
     .use(Menu)
     .use(Form)
     .use(Image)
     .use(Dropdown)
     .use(Popover)
-    //.use(Pagination)
     .use(Checkbox);
 };
